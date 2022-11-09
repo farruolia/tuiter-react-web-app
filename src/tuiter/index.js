b71895253a9dd41eb445de9a7872f8874b735df7
Farida Olia <farruolia@gmail.com>
2022-11-08 19:44:42 -0500

    test2

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,27 +1,29 @@
+import React from "react";
+import ExploreComponent from "./explore";
 import NavigationSidebar from "./navigationSidebar";
 import WhoToFollowList from "./whoToFollowList";
-import ExploreComponent from "./explore";
+import {Routes, Route} from "react-router";
 import HomeComponent from "./home";
-import {Route, Routes} from "react-router-dom";
-import {configureStore} from "@reduxjs/toolkit";
-import {Provider} from "react-redux";
-import whoReducer from "./reducers/whoReducer";
-import tuitsReducer from "./tuits/tuitsReducer";
 import ProfileComponent from "./profile";
-import profileReducer from "./profile/profileReducer";
-import EditProfile from "./profile/editProfile";
-import React from "react";
+import EditProfileComponent from "./profile/editProfile";
 
-const store = configureStore({
-    reducer: {who: whoReducer, tuits: tuitsReducer, profile: profileReducer}
-});
+import whoReducer
+    from "./reducers/whoReducer";
+import tuitsReducer from "./tuits/tuitsReducer";
+import profileReducer from "./profile/profileReducer";
+
+import { configureStore }
+    from '@reduxjs/toolkit';
+import {Provider} from "react-redux";
+const store = configureStore(
+    {reducer: {who: whoReducer, tuits: tuitsReducer, profile: profileReducer}});
 
 function Tuiter() {
-    return(
+    return (
         <Provider store={store}>
             <div className="row mt-2">
                 <div className="col-2 col-md-2 col-lg-1 col-xl-2">
-                    <NavigationSidebar/>
+                    <NavigationSidebar active="explore"/>
                 </div>
                 <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                      style={{"position": "relative"}}>
@@ -30,7 +32,7 @@ function Tuiter() {
                         <Route path="home"    element={<HomeComponent/>}/>
                         <Route path="explore" element={<ExploreComponent/>}/>
                         <Route path="profile" element={<ProfileComponent/>}/>
-                        <Route path="/edit-profile" element={<EditProfile />} />
+                        <Route path="edit-profile" element={<EditProfileComponent/>}/>
                     </Routes>
                 </div>
                 <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
@@ -40,4 +42,5 @@ function Tuiter() {
         </Provider>
     );
 }
+
 export default Tuiter
