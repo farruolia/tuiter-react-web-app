196573649d1d0264a38035510f2a9a935b931fa1
Farida Olia <farruolia@gmail.com>
2022-11-08 19:47:10 -0500

    test3

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,20 +1,18 @@
-import Nav from "../nav";
+import Nav from "../Nav";
+import {Route, Routes} from "react-router";
+import Assignment7 from "./a7/index";
 import Assignment6 from "./a6";
-import Assignment7 from "./a7";
-import {Routes, Route} from "react-router";
 
 function Labs() {
     return (
         <div>
             <Nav/>
             <Routes>
-                <Route index
+                <Route path="/"
                        element={<Assignment6/>}/>
-                <Route path="/a7/*"
-                       element={<Assignment7/>}/>
+                <Route path="a7" element={<Assignment7/>}/>
             </Routes>
         </div>
     );
 }
-
 export default Labs;
